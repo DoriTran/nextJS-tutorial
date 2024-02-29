c97cf179f936870e1dd95adc39a837a26825da56
DoriTran <tranquocdong308@gmail.com>
2024-02-29 19:23:13 +0700

    push navigate tutorial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home () {
-    return <h1>Welcome home!</h1>
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div style={{ display: "flex", flexDirection: 'column', gap: 10}}>
+            <h1>Welcome home!</h1>
+            <Link href="/blog">Blog</Link>
+            <Link href="/products">Products</Link>
+        </div>
+    );
 }
