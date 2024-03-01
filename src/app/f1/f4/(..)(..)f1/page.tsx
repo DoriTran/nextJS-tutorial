798fb0bfcc2d85dc045fd81d63f86830536a7e62
DoriTran <tranquocdong308@gmail.com>
2024-03-01 19:27:33 +0700

    route parallel intercepting

---
+++ src/app/f1/f4/(..)(..)f1/page.tsx
@@ -0,0 +1,3 @@
+export default function InterceptedF1() {
+  return <h1>(..)(..) InterceptedF1????</h1>;
+}
--- src/app/f1/f4/page.tsx
+++ src/app/f1/f4/page.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function F4() {
   return (
-    <>
+    <div style={{ display: "flex", gap: 15 }}>
       <h1>F4</h1>
+      <Link href="/f1">to F1 (not working)</Link>
+      <Link href="/f1/f2">to F2</Link>
       <Link href="/f1/f3">to F3</Link>
       <Link href="/about">to About</Link>
-    </>
+    </div>
   );
 }
